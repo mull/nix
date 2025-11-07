11feb4c69803a0525c5fb78f954b5fb78a80a063
Emil <e.ahlback@gmail.com>
2025-11-08 00:26:10 +0100

    firefox with ublock origin preinstalled D:

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -36,4 +36,22 @@
     ];
   };
 
+  programs.firefox = {
+    enable = true;
+
+    policies = {
+      # Extensions!
+      ExtensionSettings = {
+        # blocks all addons except the ones specified below
+        # dunno if we keep this
+        "*".installation_mode = "blocked";
+
+        "uBlock0@raymondhill.net" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+          installation_mode = "force_installed";
+        };
+      };
+    };
+  };
+
 }
