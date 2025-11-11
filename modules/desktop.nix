9156eda64f70a361f7d474f661049d589d8ee0c2
Emil <e.ahlback@gmail.com>
2025-11-11 09:49:22 +0100

    get on rolling release nix unstable

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -61,7 +61,7 @@
       dejavu_fonts
       noto-fonts
       noto-fonts-cjk-sans
-      noto-fonts-emoji
+      noto-fonts-color-emoji
       liberation_ttf
       font-awesome
       material-design-icons
